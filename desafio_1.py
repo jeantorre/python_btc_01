45169156d52b731d66661cc4e316adf9b362d998
jeantorre <jean.torre21@gmail.com>
2024-07-08 21:56:05 -0300

    desafio corrigido

--- desafio_1.py
+++ desafio_1.py
@@ -1,6 +1,8 @@
 nome = input('Digite seu nome: ')
 salario = int(input('Digite seu salário: '))
-bonus = 1.6
-novo_salario = salario * bonus
+bonus_inicial = float(input('Digite o bônus: '))
+bonus_inicial = bonus_inicial * 100
+bonus_final = 1 + bonus_inicial
+novo_salario = salario * bonus_final
 acrescimo = novo_salario - salario
-print(f'Neste ano tivemos um bônus de 60%!\nParabéns, {nome}! Você irá receber {novo_salario} neste mês.\nUm acréscimo de {acrescimo}!')
+print(f'Neste ano tivemos um bônus de {bonus_inicial}%!\nParabéns, {nome}! Você irá receber {novo_salario} reais neste mês.\nUm acréscimo de {acrescimo} reais!')
